f999d8172746bb51f62230a7c5d62b43525a20c1
Breno Alves Froes Peres <breno.peres@conectaservicos.com.br>
2020-04-14 14:27:58 -0300

    CRUD liquid intake

--- liquidIntake/views.py
+++ liquidIntake/views.py
@@ -17,3 +17,48 @@ def create_liquidIntake(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', ])
+def read_liquidIntake(request, id):
+    try:
+        liquid = LiquidIntake.objects.get(id=id)
+    except LiquidIntake.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = LiquidIntakeSerializer(liquid)
+        return Response(serializer.data)
+
+
+@api_view(['PUT', ])
+def update_liquidIntake(request, id):
+    try:
+        liquid = LiquidIntake.objects.get(id=id)
+    except LiquidIntake.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = LiquidIntakeSerializer(liquid, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "Update successful"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+def delete_liquidIntake(request, id):
+    try:
+        liquid = LiquidIntake.objects.get(id=id)
+    except LiquidIntake.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        operation = liquid.delete()
+        data = {}
+        if operation:
+            data["success"] = "Delete successful"
+        else:
+            data["failure"] = "Delete unsuccesful"
+        return Response(data=data)
--- personalData/urls.py
+++ personalData/urls.py
@@ -4,13 +4,14 @@ from .views import read_personalData, update_personalData, delete_personalData, 
 app_name = "personalData"
 
 urlpatterns = [
+    path('create', create_personalData, name='create'),
     path('<id>/', read_personalData, name='detail'),
     path('edit/<id>', update_personalData, name='edit'),
     path('delete/<id>', delete_personalData, name='delete'),
-    path('create', create_personalData, name='create'),
+
+    path('phone/create', create_phone, name='createPhone'),
     path('phone/<id>/', read_phone, name='detailPhone'),
     path('phone/edit/<id>', update_phone, name='editPhone'),
     path('phone/delete/<id>', delete_phone, name='deletePhone'),
-    path('phone/create', create_phone, name='createPhone'),
 
 ]
