372df2b473506708c512b4a3f6dbb4808f5fe203
Breno Alves Froes Peres <breno.peres@conectaservicos.com.br>
2020-04-14 13:37:57 -0300

    Ajuste do personal data

--- personalData/serializers.py
+++ personalData/serializers.py
@@ -3,12 +3,25 @@ from .models import PersonalData, Phone
 from django.contrib.auth.models import User
 
 class PersonalDataSerializer(serializers.ModelSerializer):
+
+    email = serializers.SerializerMethodField('get_email_from_user')
+
+    phone = serializers.SerializerMethodField('get_number_from_phone')
+
     class Meta:
         model = PersonalData
         depth = 1
-        fields = ['id', 'name', 'birthDate', 'gender', 'cpf', 'rg', 'addressNumber', 'addressComplement',
-                  'healthEnsurance', 'healthEnsuranceCompany', 'healthEnsuranceDescription', 'hasHealthData',
-                  'hasPersonalData', 'hasPrescription', 'hasQrMedication']
+        fields = ['id', 'name', 'email', 'birthDate', 'gender', 'phone', 'cpf', 'rg', 'cep', 'addressNumber', 'addressComplement',
+                  'addressStreet', 'addressCity', 'addressDistrict', 'addressFederalState', 'healthEnsurance', 'healthEnsuranceCompany',
+                  'healthEnsuranceDescription', 'hasHealthData', 'hasPersonalData', 'hasPrescription', 'hasQrMedication']
+
+    def get_email_from_user(self, personal):
+        email = personal.user.email
+        return email
+
+    def get_number_from_phone(self, personal):
+        number = personal.phone.number
+        return number
 
 class PhoneSerializer(serializers.ModelSerializer):
 
