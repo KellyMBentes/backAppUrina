a861e5f2d1a65c6e07ca6574bacf3c13b7cf3310
arturladeira <arturladeira@id.uff.br>
2020-04-15 12:15:48 -0300

    crud notification

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,5 +5,13 @@ from notifications import views
 app_name = 'notification'
 
 urlpatterns = [
-    path('create', views.create_notification, name="create")
+    path('<int:pk>', views.get_notification, name="get"),
+    path('create', views.create_notification, name="create"),
+    path('update/<int:pk>', views.update_notification, name="update"),
+    path('delete/<int:pk>', views.delete_notification, name="delete"),
+    path('option/<int:pk>', views.get_option, name="get"),
+    path('option/create', views.create_option, name="create"),
+    path('option/update/<int:pk>', views.update_option, name="update"),
+    path('option/delete/<int:pk>', views.delete_option, name="delete")
 ]
+
