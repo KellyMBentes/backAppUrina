86e5c2809729d07ffe172989da65cc27571a2218
Victor Matheus  Pereira De Azevedo <victor.azevedo@ConectaSa.com.br>
2020-05-04 11:00:57 -0300

    introdução de offset e limit no chat

--- chat/views.py
+++ chat/views.py
@@ -1,24 +1,70 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.decorators import api_view
 
 from chat.models import Message
+from drf_yasg import openapi
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from chat.serializers import MessageSerializer
 from django.views.decorators.csrf import csrf_exempt
 
+chat_response = openapi.Response('OK', MessageSerializer)
+
 
 # Create your views here.
+@swagger_auto_schema(method='get', responses={
+        '200': chat_response,
+        '400': 'Bad Request',
+        '401': 'Unauthorized',
+        '404': 'Not Found',
+    })
 @api_view(['GET', ])
-def message_list(request,receiver=None):
+def message_list(request, receiver=None):
+    global content
     if request.method == 'GET':
         user = request.user
-        print(user)
-        messages = Message.objects.filter(sender_id=user, receiver_id=receiver) | Message.objects.filter(sender_id=receiver, receiver_id=user)
+        offset = request.query_params.get('offset', None)
+        limit = request.query_params.get('limit', None)
+        data = {}
 
-        serializer = MessageSerializer(messages, many=True, context={'request': request})
-        return JsonResponse(serializer.data, safe=False)
+        if offset is not None and limit is not None:
+            offset = int(offset)
+            limit = int(limit)
+            if offset >= limit:
+                data['error'] = "offset param must be less than limit param."
+                return JsonResponse(data=data, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                messages = Message.objects.filter(sender_id=user, receiver_id=receiver).order_by("-timestamp") | Message.objects.filter(
+                    sender_id=receiver, receiver_id=user).order_by("-timestamp")
+
+                content = messages.filter()[offset:limit]
+        elif offset is None and limit is not None :
+            limit = int(limit)
+            messages = Message.objects.filter(sender_id=user, receiver_id=receiver).order_by(
+                "-timestamp") | Message.objects.filter(
+                sender_id=receiver, receiver_id=user).order_by("-timestamp")
+            content = messages.filter()[:limit]
+        elif offset is not None and limit is None:
+            data['error'] = "offset param requires a limit param."
+            return JsonResponse(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.method == 'GET':
+            serializer = MessageSerializer(content, many=True, context={'request': request})
+            if serializer.data.__len__() == 0:
+                data["end"] = "don't have new messages yet"
+                return JsonResponse(data=data, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return JsonResponse(serializer.data, safe=False)
 
 
+
+@swagger_auto_schema(method='post', request_body=MessageSerializer,
+                     responses={
+                         '201': 'Created',
+                         '400': 'Bad Request',
+                         '401': 'Unauthorized',
+                     })
 @api_view(['POST', ])
 def message_post(request, sender=None, receiver=None):
     if request.method == 'POST':
