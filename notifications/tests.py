e6f2967c35086a642b5411c2e345b3201ac4a316
arturladeira <arturladeira@id.uff.br>
2020-05-08 12:23:23 -0300

    criado POST e GET em notifications, peeDiary e liquidIntake. Iniciado o POST e GET em personalData

--- notifications/tests.py
+++ notifications/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import CustomUser
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_notification(self):
+
+        # Create user
+
+        url_register = "http://127.0.0.1:8000/api/user/register"
+        data = {'email': 'nofication@gmail.com', 'password': '123', 'confirm_password': '123'}
+        response = self.client.post(url_register, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(CustomUser.objects.get().email, 'nofication@gmail.com')
+
+        # Login
+
+        user = CustomUser.objects.get(email='nofication@gmail.com')
+        user.is_active = True
+        user.save()
+        url_login = "http://127.0.0.1:8000/api/user/login"
+        data = {'email': 'nofication@gmail.com', 'password': '123'}
+        response = self.client.post(url_login, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Authorization
+
+        token = response.data.get('token')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        user.save()
+
+        # Post Notification
+
+        url = "http://127.0.0.1:8000/api/notification/create"
+        data = {'isSelectedAll': True}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Get Notification
+
+        url = "http://127.0.0.1:8000/api/notification/1"
+        response = self.client.get(url, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
--- peeDiary/tests.py
+++ peeDiary/tests.py
@@ -1,37 +1,36 @@
-
 from rest_framework import status
 from rest_framework.test import APITestCase
 from users.models import CustomUser
 
 
 class AccountTests(APITestCase):
-    def test_create_account(self):
+    def test_liquid_intake(self):
 
-        #Create user
+        # Create user
 
         url_register = "http://127.0.0.1:8000/api/user/register"
-        data = {'email': 'name@gmail.com','password':'123','confirm_password':'123'}
+        data = {'email': 'pee@gmail.com', 'password': '123', 'confirm_password': '123'}
         response = self.client.post(url_register, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(CustomUser.objects.get().email, 'name@gmail.com')
+        self.assertEqual(CustomUser.objects.get().email, 'pee@gmail.com')
 
-        #Login
+        # Login
 
-        user = CustomUser.objects.get(id=1)
+        user = CustomUser.objects.get(email='pee@gmail.com')
         user.is_active = True
         user.save()
         url_login = "http://127.0.0.1:8000/api/user/login"
-        data = {'email': 'name@gmail.com', 'password': '123'}
+        data = {'email': 'pee@gmail.com', 'password': '123'}
         response = self.client.post(url_login, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        #Authorization
+        # Authorization
 
         token = response.data.get('token')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
         user.save()
 
-        #Post PeeDiary
+        # Post PeeDiary
 
         url = "http://127.0.0.1:8000/api/peeDiary/create"
         data = {'peeVolume': 100, 'effortToUrinate': 110,
